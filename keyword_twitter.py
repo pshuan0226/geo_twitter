70953bce8cd3cfc74ebcabc043e32c6f092d5268
pshuan0226 <siahuang@umich.edu>
2018-06-19 21:51:46 -0400

    geocode on

--- keyword_twitter.py
+++ keyword_twitter.py
@@ -9,13 +9,13 @@ auth.set_access_token(config["access_key"], config["access_secret"])
 
 api = tweepy.API(auth)
 
-query = ('Blue Cross OR bcbsil')
-max_result = 100
+query = ('bcbsil OR HCSC -filter:retweets')
+max_result = 1000
 result_count = 0
 search_result = []
 last_id = -1
 
-search_result = [status for status in tweepy.Cursor(api.search, q=query, tweet_mode = "extended").items(max_result)]
+search_result = [status for status in tweepy.Cursor(api.search, q=query, lang="en", geocode="41.884938,-87.619960,20km", tweet_mode="extended").items(max_result)]
 
 result_count = len(search_result)
 
